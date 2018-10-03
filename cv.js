680669759f446507edf97629256f7ae6b9058652
Vonhaiklei <pauline.gorecka@gmail.com>
2018-10-04 01:35:59 +0300

    beams and gif

--- cv.js
+++ cv.js
@@ -1,8 +1,10 @@
+
 window.onload = function() {
-let canvas = document.getElementById('stairsCanvas');
-let ctx = canvas.getContext("2d");
-let img = document.getElementById("stairsImg");
-let img2 = this.document.getElementById("stairsImg2");
+
+    let canvas = document.getElementById('stairsCanvas');
+    let ctx = canvas.getContext("2d");
+    let img = document.getElementById("stairsImg");
+    let img2 = this.document.getElementById("stairsImg2");
 
     if (this.screen.width <= 320) {
         canvas.width = 320;
@@ -22,9 +24,9 @@ let img2 = this.document.getElementById("stairsImg2");
     }
     else if (screen.width <= 1024) {
         canvas.width = 450;
-        ctx.drawImage(img, 140, 325, 257, 240); 
+        ctx.drawImage(img, 140, 326, 257, 240); 
     }
-    else if (this.screen.width <= 1366) {
+    else if (screen.width <= 1366) {
         canvas.width = 630;
         ctx.drawImage(img, 212, 326, 257, 240);
     }
@@ -34,7 +36,107 @@ let img2 = this.document.getElementById("stairsImg2");
     }
 }
 
+window.onresize = function() { 
+    location.reload(); 
+}
+
+window.onclick = function() {
+
+    let canvas = document.getElementById('mainCanvas');
+    
+        if (this.screen.width <= 320) {
+            canvas.width = 220;
+           //ctx.drawImage(img2, -21, 285, 340, 310);    
+        }
+        else if (screen.width <= 375) {
+            canvas.width = 270;
+           // ctx.drawImage(img2, -4, 255, 380, 350);    
+        }
+        else if (screen.width <= 425) {
+            canvas.width = 200;
+           // ctx.drawImage(img2, 3, 237, 410, 380); 
+        }
+        else if (screen.width <= 768) {
+            canvas.width = 200;
+           // ctx.drawImage(img, 118, 326, 258, 240);
+        }
+        else if (screen.width <= 1024) {
+            canvas.width = 600;
+           // ctx.drawImage(img, 140, 326, 257, 240); 
+        }
+        else if (screen.width <= 1366) {
+            canvas.width = 600;
+          //  ctx.drawImage(img, 212, 326, 257, 240);
+        }
+        else if (screen.width > 1366) {
+            canvas.width = 630;
+          //  ctx.drawImage(img, 240, 325, 257, 240);
+        }
+    }
+
+$(document).ready(function() {
+
+//const animateStairs = function() {
+let $beamLeft = $('.beamLeft');
+//let $beamRight = $('.beamRight');
+let $background = $('.background');
+let $photo = $('.photoIdImg'); 
+
+$('body').click(function() {
+    $beamLeft.fadeOut(2500);
+   // $beamRight.fadeOut(2500);
+    $photo.fadeOut(2500).removeClass('photoIdImg').addClass('photoIdMask');
+
+    setTimeout(function() {
+        $background.css('backgroundImage','url(https://i.imgur.com/o6ILxdK.jpg)');
+    }, 2000);
+
+    if (screen.width <= 425) {
+        $('#stairsCanvas').fadeOut(2000);
+    }
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+   /*else if (screen.width > 425) {
+        animateStairs();
+    });
+    } */
+
+
+    });
+
+});
+
+
+
+
+
+//var typed = new Typed('.element', {
+ //   strings: ["First sentence.", "Second sentence."],
+ //   typeSpeed: 30
+ // });
+
+
 /*var typed = new Typed('.mainScreen', {
+
+
+
+
+
+
     strings: ["First sentence.", "Second sentence."],
     typeSpeed: 30
   }); */
